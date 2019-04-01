f09ec01aed5337f2fa9a2765c027b35f01869669
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2019-04-01 22:17:42 +0200

    config: restart sshd after config
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- configuration.sh
+++ configuration.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "[SSH] Disable password login"
+echo "[SSH] disable password login"
 [ -z `cat /etc/ssh/sshd_config | grep ^PasswordAuthentication` ] && echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 
-echo "[SSH] Client alive interval: 120"
+echo "[SSH] client alive interval: 120"
 [ -z `cat /etc/ssh/sshd_config | grep ^ClientAliveInterval` ] && echo "ClientAliveInterval 120" >> /etc/ssh/sshd_config
+
+echo "[SSH] reload server"
+service ssh restart
