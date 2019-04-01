54c0fb0df10d17a6f6ad2b9db4effc358d02fc22
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2019-04-02 00:24:32 +0200

    config: config page up/down
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- configuration.sh
+++ configuration.sh
@@ -16,3 +16,10 @@ cp config/fail2ban/jail.local /etc/fail2ban
 
 echo "[Fail2ban] reload server"
 service fail2ban restart
+
+#####
+
+echo "[Inpurc] Configure page-up/down"
+echo "" >> /etc/inputrc
+echo '"\e[5~": history-search-backward' >> /etc/inputrc
+echo '"\e[6~": history-search-forward' >> /etc/inputrc
