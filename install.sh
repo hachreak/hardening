a834532e1cb4a0ddf87f7096979047755605e865
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2019-04-01 20:47:50 +0200

    provisioning: create user/ssh key
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y fail2ban
+apt-get install -y fail2ban make
--- provisioning.sh
+++ provisioning.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+DUSER=${DUSER:-hachreak}
+
+echo "Create default group [$DUSER]"
+[ -z `groups | grep $DUSER` ] && groupadd -r $DUSER -g 1000
+echo "Create default user [$DUSER]"
+[ -z `cat /etc/passwd| grep $DUSER` ] && useradd -d /home/$DUSER -m -u 1000 -r -g $DUSER $DUSER
+
+
 echo "SSH key generator.."
-ssh-keygen -t rsa -b 4096
+! [ -f /home/$DUSER/.ssh/id_rsa ] && runuser $DUSER -c "ssh-keygen -t rsa -b 4096 -q -f /home/$DUSER/.ssh/id_rsa_test -N \"\""
