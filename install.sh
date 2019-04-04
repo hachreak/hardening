da899b0b63faa6bf4661624a587a06d7f66f14e7
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2019-04-05 01:14:49 +0200

    install: add openssl-dev
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 apt-get update
 apt-get install -y fail2ban make openssh-server byobu gcc g++ make \
-  build-essential libncurses5-dev curl munin munin-plugins-extra munin-node
+  build-essential libncurses5-dev curl munin munin-plugins-extra munin-node \
+  libssl-dev
