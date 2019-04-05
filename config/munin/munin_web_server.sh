01344253b4e13547fe29c8610ece1dee1122f1e7
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2019-04-05 17:04:22 +0200

    config: munin user/pwd
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- config/munin/munin_web_server.sh
+++ config/munin/munin_web_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /var/cache/munin/www
-python ~/bin/auth-http-server.py 5050 "$1:$2" 2> /dev/null > /dev/null &
+python ~/bin/auth-http-server.py 5050 "$MUNIN_USER:$MUNIN_PWD" 2> /dev/null > /dev/null &
 
